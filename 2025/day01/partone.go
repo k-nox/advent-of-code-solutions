1c84e3304c1d8b826d09f00bbb57c56f20c22322
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-12-01 17:40:14 -0800

    solve year 2025 day 1 part 1

--- 2025/day01/partone.go
+++ 2025/day01/partone.go
@@ -3,6 +3,8 @@ package day01
 import (
 	"bufio"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/k-nox/advent-of-code-solutions/helper"
 )
@@ -17,8 +19,43 @@ func PartOne(useSample bool) int {
 
 	scanner := bufio.NewScanner(f)
 
+	dial := 50
+	zeroCount := 0
+
 	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		count := decode(line)
+		dial = rotate(dial, count)
+		if dial == 0 {
+			zeroCount++
+		}
 	}
 
-	return 0
+	return zeroCount
+}
+
+func rotate(from int, count int) int {
+	boundedCount := count % 100
+	result := from + boundedCount
+	if result < 0 {
+		result = 100 + result
+	} else if result >= 100 {
+		result = result - 100
+	}
+
+	return result
+}
+
+func decode(line string) int {
+	if len(line) == 0 {
+		return 0
+	}
+	num, err := strconv.Atoi(line[1:])
+	if err != nil {
+		panic(fmt.Errorf("line not convertible to number: %s: %w", line, err))
+	}
+	if line[0] == 'L' {
+		return num * -1
+	}
+	return num
 }
