4652e6fc2e9e333047553878fa3a8da341d1935d
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-12-03 17:41:22 -0800

    solve year 2025 day 3 part 2

--- 2025/day03/parttwo.go
+++ 2025/day03/parttwo.go
@@ -2,7 +2,9 @@ package day03
 
 import (
 	"bufio"
-  "fmt"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/k-nox/advent-of-code-solutions/helper"
 )
@@ -17,9 +19,42 @@ func PartTwo(useSample bool) int {
 
 	scanner := bufio.NewScanner(f)
 
+	totalJoltage := 0
 	for scanner.Scan() {
-	
+		bankRaw := strings.TrimSpace(scanner.Text())
+		if len(bankRaw) > 0 {
+			totalJoltage += calculateJoltagePartTwo([]rune(bankRaw))
+		}
 	}
 
-	return 0
+	return totalJoltage
+}
+
+func calculateJoltagePartTwo(bank []rune) int {
+	joltageRaw := []rune{}
+	lastBatteryIdx := -1
+	for len(joltageRaw) < 12 {
+		nextBatteryIdx := findNextBattery(bank, lastBatteryIdx, 12-len(joltageRaw))
+		nextBattery := bank[nextBatteryIdx]
+		joltageRaw = append(joltageRaw, nextBattery)
+		lastBatteryIdx = nextBatteryIdx
+	}
+
+	joltage, err := strconv.Atoi(string(joltageRaw))
+	if err != nil {
+		fmt.Printf("something weird happened trying to convert joltage: %s: %s\n", string(joltageRaw), err)
+	}
+	return joltage
+}
+
+func findNextBattery(bank []rune, lastBatteryIdx int, maxLen int) int {
+	maxBatteryIdx := lastBatteryIdx + 1
+	if bank[maxBatteryIdx] != '9' {
+		for idx := maxBatteryIdx + 1; idx <= len(bank)-maxLen; idx++ {
+			if bank[idx] > bank[maxBatteryIdx] {
+				maxBatteryIdx = idx
+			}
+		}
+	}
+	return maxBatteryIdx
 }
