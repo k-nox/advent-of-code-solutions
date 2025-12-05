916f2ba63128f07f7c9f6111c96b21239f4a18c2
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-12-04 18:22:41 -0800

    solve year 2025 day 4 part 2

--- 2025/day04/parttwo.go
+++ 2025/day04/parttwo.go
@@ -2,7 +2,8 @@ package day04
 
 import (
 	"bufio"
-  "fmt"
+	"fmt"
+	"image"
 
 	"github.com/k-nox/advent-of-code-solutions/helper"
 )
@@ -17,9 +18,38 @@ func PartTwo(useSample bool) int {
 
 	scanner := bufio.NewScanner(f)
 
-	for scanner.Scan() {
-	
+	grid := parseGrid(scanner)
+
+	totalRemoved := 0
+	for grid.Len() > 0 {
+		var removedCount int
+		grid, removedCount = removeRolls(grid)
+		if removedCount == 0 {
+			break
+		}
+		totalRemoved += removedCount
 	}
 
-	return 0
+	return totalRemoved
+}
+
+func removeRolls(grid helper.Set[image.Point]) (helper.Set[image.Point], int) {
+	removedCount := 0
+	rolls := grid.Members()
+	for _, roll := range rolls {
+		otherRollsCount := 0
+		for _, direction := range helper.AllDirections {
+			if otherRollsCount == 4 {
+				break
+			}
+			if grid.Contains(direction(roll)) {
+				otherRollsCount++
+			}
+		}
+		if otherRollsCount < 4 {
+			grid.Remove(roll)
+			removedCount++
+		}
+	}
+	return grid, removedCount
 }
